32bd7c6062ea7e01f83d3e217931073dd50bce0e
Leo Cassarani <leo.cassarani@me.com>
2012-05-15 11:35:26 +0100

    Write a failing Cucumber feature for entry submission

---
+++ features/entry_submission.feature
@@ -0,0 +1,12 @@
+Feature: Users can submit an entry
+  In order to share something on the web
+  As a user
+  I want to submit an entry to the site
+
+  @wip
+  Scenario: Successful submission
+    When I visit the entry submission form
+    And  I enter a URL and a title
+    And  I enter my email address
+    And  I submit my entry
+    Then my entry should be created
---
+++ features/step_definitions/entry_steps.rb
@@ -0,0 +1,19 @@
+When /^I visit the entry submission form$/ do
+  visit new_entry_path
+end
+
+When /^I enter a URL and a title/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I enter my email address$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I submit my entry$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^my entry should be created$/ do
+  pending # express the regexp above with the code you wish you had
+end
