8cd5daecee34d742eadc07f29e08d5fd279c97c1
Leo Cassarani <leo.cassarani@me.com>
2012-05-15 10:29:54 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'mysql2'
+
+group :assets do
+  gem 'therubyracer', :platform => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlailsToRails::Application.config.secret_token = 'a67ec78b1b1cdf0f7d8195bb0d26fbd84971585b2cb8ff8552bc5eed6b5ab7d4598738f8a105760d8f0fdb60e925e66741ccd9007a9b5ed133c2c1edd858a665'
