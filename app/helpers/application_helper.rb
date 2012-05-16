0fedcc5505e87170400fc9f74ed497ff8921d7a9
Leo Cassarani <leo.cassarani@me.com>
2012-05-16 14:19:49 +0100

    Move navbar items to a helper method, style the current one as active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def navbar_item(title, path)
+    klass = request.path == path ? 'active' : nil
+    content_tag :li, :class => klass do
+      link_to title, path
+    end
+  end
 end
